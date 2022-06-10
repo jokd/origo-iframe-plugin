10923fdb347d6257ab73c7bfb02ef1a7fd6205ea
Jan Dalheimer <jan.dalheimer@sweco.se>
2022-06-10 16:16:13 +0200

    Setup prettier & eslint

--- src/origoiframeetuna.js
+++ src/origoiframeetuna.js
@@ -5,9 +5,7 @@ import GeoJSON from 'ol/format/GeoJSON';
  * @param {{ layerName: string, layerIDField: string, maxZoom: number=, zoomDuration: number= }} options
  */
 const Origoiframeetuna = function Origoiframeetuna(options = {}) {
-  const {
-    layerName, layerIDField, maxZoom, zoomDuration
-  } = options;
+  const {layerName, layerIDField, maxZoom, zoomDuration} = options;
 
   let viewer;
 
@@ -20,7 +18,7 @@ const Origoiframeetuna = function Origoiframeetuna(options = {}) {
   function applyFiltering() {
     const layer = viewer.getLayer(layerName);
     if (layer.get('type') === 'WMS') {
-      layer.getSource().updateParams({ CQL_FILTER: cqlFilter });
+      layer.getSource().updateParams({CQL_FILTER: cqlFilter});
     } else {
       console.warn('Layer of type', layer.get('type'), 'is not supported for iframe filtering');
     }
@@ -40,7 +38,9 @@ const Origoiframeetuna = function Origoiframeetuna(options = {}) {
     url.searchParams.set('outputFormat', 'application/json');
     url.searchParams.set('typeNames', layerName);
     url.searchParams.set('cql_filter', `${layerIDField} = '${id}'`);
-    return fetch(url.toString()).then(res => res.json()).then(data => new GeoJSON().readFeatures(data)[0]);
+    return fetch(url.toString())
+      .then(res => res.json())
+      .then(data => new GeoJSON().readFeatures(data)[0]);
   }
 
   /**
@@ -53,26 +53,34 @@ const Origoiframeetuna = function Origoiframeetuna(options = {}) {
     if (layer.get('type') === 'WMS') {
       // this works for geoserver, but might not for others
       const source = layer.getSource();
-      const url = new URL(typeof source.getUrl === 'function' ? source.getUrl() : source.getUrls()[0]);
+      const url = new URL(
+        typeof source.getUrl === 'function' ? source.getUrl() : source.getUrls()[0]
+      );
       url.pathname = url.pathname.replace('wms', 'wfs');
       return getFeatureFromWFS(url, id);
-    } else if (layer.get('type') === 'WFS') {
+    }
+    if (layer.get('type') === 'WFS') {
       // warning: code path not tested
       // best case - we already have the feature
-      const feature = layer.getSource().getFeatures().find(f => f.get(layerIDField) === id);
+      const feature = layer
+        .getSource()
+        .getFeatures()
+        .find(f => f.get(layerIDField) === id);
       if (feature) {
         return Promise.resolve(feature);
       }
       // otherwise, we need to fetch it
       return getFeatureFromWFS(new URL(layer.getSource().getUrl()), id);
     }
-    throw new Error(`Layer of type ${layer.get('type')} is not supported for iframe feature pan/zoom`);
+    throw new Error(
+      `Layer of type ${layer.get('type')} is not supported for iframe feature pan/zoom`
+    );
   }
 
   return Origo.ui.Component({
     name: 'origoiframeetuna',
     onInit() {
-      window.addEventListener('message', (event) => {
+      window.addEventListener('message', event => {
         if (typeof event.data !== 'string' || (event.data.match(/:/g) || []).length !== 1) {
           console.warn('Received a message with an invalid format. Expected <command>:<payload>');
           return;
@@ -84,7 +92,10 @@ const Origoiframeetuna = function Origoiframeetuna(options = {}) {
           applyFiltering();
         } else if (command === 'setVisibleIDs') {
           // command to filter by the ID field
-          cqlFilter = `${layerIDField} IN (${payload.split(',').map(id => `'${id}'`).join(', ')})`;
+          cqlFilter = `${layerIDField} IN (${payload
+            .split(',')
+            .map(id => `'${id}'`)
+            .join(', ')})`;
           applyFiltering();
         } else if (command === 'resetFilter') {
           // command to reset the filter, showing all features
@@ -92,12 +103,29 @@ const Origoiframeetuna = function Origoiframeetuna(options = {}) {
           applyFiltering();
         } else if (command === 'panTo') {
           // command to pan to a feature by id
-          getFeature(payload).then(f => viewer.getMap().getView().setCenter(f.getGeometry().getExtent().getCenter()));
+          getFeature(payload).then(f =>
+            viewer
+              .getMap()
+              .getView()
+              .setCenter(
+                f
+                  .getGeometry()
+                  .getExtent()
+                  .getCenter()
+              )
+          );
         } else if (command === 'zoomTo') {
           // command to zoom to a feature by id
-          getFeature(payload).then(f => viewer.getMap().getView().fit(f.getGeometry(), { maxZoom, duration: zoomDuration }));
+          getFeature(payload).then(f =>
+            viewer
+              .getMap()
+              .getView()
+              .fit(f.getGeometry(), {maxZoom, duration: zoomDuration})
+          );
         } else {
-          console.warn('Received a message with an invalid command. Expected setFilter|setVisibleIDs|resetVisible|panTo|zoomTo.');
+          console.warn(
+            'Received a message with an invalid command. Expected setFilter|setVisibleIDs|resetVisible|panTo|zoomTo.'
+          );
         }
       });
     },
@@ -105,15 +133,18 @@ const Origoiframeetuna = function Origoiframeetuna(options = {}) {
       viewer = evt.target;
 
       // in case the layer gets added _after_ we have received a message
-      viewer.getMap().getLayers().on('add', async ({ element }) => {
-        if (element.get('name')) {
-          applyFiltering();
-        }
-      });
+      viewer
+        .getMap()
+        .getLayers()
+        .on('add', async ({element}) => {
+          if (element.get('name')) {
+            applyFiltering();
+          }
+        });
     },
     render() {
       // no-op
-    }
+    },
   });
 };
 
